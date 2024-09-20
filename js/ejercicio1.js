93f4709ce7184443901435e8689f2886c2afe276
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-09-20 10:38:24 +0200

    commit

---
+++ js/ejercicio1.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',function(){
+    var hipervinculo =  document.getElementById('hipervinculo');
+    hipervinculo.textContent = "Google";
+    hipervinculo.addEventListener('hover',function(){
+        
+    })
+});
