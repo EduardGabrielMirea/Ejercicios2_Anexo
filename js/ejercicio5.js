4b1b3d0581f28523ab394d0cddd82ea7d59d518d
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-09-26 11:26:21 +0200

    commit

---
+++ js/ejercicio5.js
@@ -0,0 +1,5 @@
+function redirigir() {
+    setTimeout(function() {
+        window.location.href = 'nueva_pagina.html'; // Redirige a la nueva página
+    }, 20000); // 20000 milisegundos = 20 segundos
+}
