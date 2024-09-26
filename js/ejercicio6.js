f33b1388f05e6a1673bfda3260263776522776e4
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-09-26 11:48:06 +0200

    commit

---
+++ js/ejercicio6.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    let cadena = "2+2";
+    let resultado = eval(cadena); // Ejecuta la cadena "2+2"
+    alert("El resultado de la suma es: " + resultado); // Muestra el resultado
+};
